2e9545a56351695481d6d2dcbfb76cf158de394b
ananyanivarthi <ananyanivarthi@gmail.com>
2024-12-22 09:36:21 +0530

    files changed

--- template.py
+++ template.py
@@ -2,37 +2,45 @@ import logging
 import os
 from pathlib import Path
 
-list_of_files=[
-    ".github/workflow/.gitkeep",
-    "src/__init__.py",
-    "src/components/data_ingestion.py",
-    "src/components/data_transformation.py",
-    "src/components/model_trainer.py",
-    "src/components/model_evaluation.py",
-    "src/pipeline/__init__.py",
-    "src/pipeline/training_pipeline.py",
-    "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py",
-    "src/utils/utils.py",
-    "src/logger/logging.py",
-    "src/exception/exception.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
+project_name="us_visa"
+list_of_files= [
+
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
     "requirements.txt",
-    "requirements_dev.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
     "setup.py",
-    "setup.config",
-    "pyproject.toml",
-    "tox.ini",
-    "experiment/experiments.ipynb"
+    "config/model.yaml",
+    "config/schema.yaml",
 ]
 for filepath in list_of_files:
     filepath=Path(filepath)
     filedir,filename=os.path.split(filepath)
     if filedir!="":
         os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory:{filedir} for file:{filename}")
     if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
             pass
+    else:
+        print(f"file is already present at:{filepath}")
